40c73ea46045d1b9065c1cc11ef8c4f08bff1f39
fangwl591021 <50135766+fangwl591021@users.noreply.github.com>
2026-09-01 15:25:20 +0800

    style: load desktop rich menu library styles

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import Root from './Root';
 import './styles.css';
 import './mobile.css';
 import './mobile-library.css';
+import './desktop-library.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
